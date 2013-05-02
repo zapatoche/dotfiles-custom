074cc11ca9654aa5e6a1970604b4eb112855c950
Yannick Schall <yannick@bishbashbosh.it>
2013-05-02 16:26:57 +0100

    modified colorscheme

--- zsh/after/aliases.zsh
+++ zsh/after/aliases.zsh
@@ -17,25 +17,5 @@ alias scn='screen -S'
 alias sck='screen -X -S '
 alias scl='screen -list'
 
-# Gem install
-alias gemi='gem install --no-ri --no-rdoc'
-alias rgi='rvm all do gem install --no-ri --no-rdoc'
-
-
 # finder  
 alias ql='qlmanage -p'
-alias rnmr='/usr/local/Cellar/rename/0.1.3/bin/rename'
-alias trre='/usr/local/Cellar/tree/1.6.0/bin/tree'
-alias aack='/usr/local/Cellar/ack/1.96/bin/ack '
-alias 2012='/Volumes/creative3$/ONLINE_WORK/2012/PartyCasino.com/'
-
-
-# sync most used folders
-alias cpd='rsync -vax --delete --progress ~/Desktop/ /Volumes/yannicks\$/Desktop'
-alias cpg='rsync -vax --delete --progress ~/github/ /Volumes/yannicks\$/github'
-alias cpapp='rsync -vax --delete --progress ~/Applications/ /Volumes/yannicks$/Applications/'
-alias cpaps='rsync -vax --delete --progress ~/Library/Application\ Support/ /Volumes/yannicks$/Library/Application\ Support/'
-alias cpy='rsync -vax --delete --progress ~/.yadr/ /Volumes/yannicks$/.yadr/'
-
-
-
--- zsh/before/home-config.zsh
+++ zsh/before/home-config.zsh
@@ -1,21 +1,17 @@
-# Give me my bash style incremental search
-bindkey '^R' history-incremental-search-backward
-
+# [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 #Add path to our custom bins
-export PATH=/Users/yannickschall/.rvm/gems/ruby-1.9.3-p0/bin:/Users/yannickschall/.rvm/gems/ruby-1.9.3-p0@global/bin:/Users/yannickschall/.rvm/rubies/ruby-1.9.3-p0/bin:/Users/yannickschall/.rvm/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin
+PATH=$PATH:/usr/local/share/npm/bin
+export PATH=/Applications/Postgres.app/Contents/MacOS/bin:$PATH
 export PATH=$PATH:$yadr/bin:$yadr/bin/yadr
+# export PATH=$PATH:/Applications/MAMP/Library/bin
+# export PATH=$PATH:/Applications/MAMP/bin
+# export PATH=$PATH:/usr/local/Cellar/git
 
 #Always pushd when changing directory
 setopt auto_pushd
 
-setopt auto_cd
-cdpath=($HOME $HOME/Desktop/bwin.party $HOME/github $HOME/Dropbox $HOME/Dropbox/projects $HOME/projects)
+# setopt auto_cd
+cdpath=($HOME  $HOME/Dropbox $HOME/Dropbox/projects $HOME/projects)
 
 # Show completion on first TAB
 setopt menucomplete
-
-# Fuzzy matching of completions for when you mistype them:
-# zstyle ':completion:*' completer _complete _match _approximate
-# zstyle ':completion:*:match:*' original only
-# zstyle ':completion:*:approximate:*' max-errors 1 numeric'
-
