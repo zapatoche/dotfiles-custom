01d86ee786f44272d0597de7801e766e626973b7
Yannick Schall <yannick.schall@mvfglobal.com>
2016-02-03 10:50:48 +0000

    Cleanup $PATH

--- zsh/before/home-config.zsh
+++ zsh/before/home-config.zsh
@@ -1,11 +1,9 @@
 # [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 #Add path to our custom bins
 PATH=$PATH:/usr/local/share/npm/bin
-export PATH=/Applications/Postgres.app/Contents/MacOS/bin:$PATH
+# export PATH=/Applications/Postgres.app/Contents/MacOS/bin:$PATH
 export PATH=$PATH:$yadr/bin:$yadr/bin/yadr
-# export PATH=$PATH:/Applications/MAMP/Library/bin
-# export PATH=$PATH:/Applications/MAMP/bin
-# export PATH=$PATH:/usr/local/Cellar/git
+export PATH=$PATH:~/.composer/vendor/bin
 
 #Always pushd when changing directory
 setopt auto_pushd
@@ -15,3 +13,7 @@ cdpath=($HOME  $HOME/Dropbox $HOME/Dropbox/projects $HOME/projects)
 
 # Show completion on first TAB
 setopt menucomplete
+
+unalias run-help
+autoload run-help
+HELPDIR=/usr/local/share/zsh/help
